d6e2e3405a9b52e54aef4d44cb7a744f5f96f6bb
corey sloan <sloancb@mail.uc.edu>
2025-04-28 19:18:09 -0400

    genuinely fixed on main

--- app.py
+++ app.py
@@ -851,4 +851,6 @@ def index():
  # Main
  # ==================================
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug=True,port=5001)
